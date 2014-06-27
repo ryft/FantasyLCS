223be50ec1bde335c289e0fe260a061896307c5d
James Nicholls <thejamesnicholls@gmail.com>
2014-06-27 14:30:16 +0000

    Add API functions to return weekly fantasy scores
    Also allowed date bound parameters on teamGame / playerGame
    Fixed bug where only 1 result was returned from a query

--- www/api/Fantasy/API.pm
+++ www/api/Fantasy/API.pm
@@ -1,6 +1,7 @@
 package Fantasy::API;
 
 use Data::Dumper;
+use DateTime;
 use DBI;
 use Moose;
 
@@ -57,26 +58,116 @@ sub player {
     return \@player;
 }
 
+sub team_game {
+    my ($self, $params) = @_;
+    my $dbh = $self->dbh;
+
+    # TODO There MUST be a better way to do this.
+    if ($params->{gameId}) {
+        if ($params->{start} && $params->{end}) {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM `match` m, game g, teamGame tg WHERE tg.gameId = ? AND tg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ? AND m.`dateTime` < ?', {}, $params->{gameId}, $params->{start}, $params->{end});
+        } elsif ($params->{start}) {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM `match` m, game g, teamGame tg WHERE tg.gameId = ? AND tg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ?', {}, $params->{gameId}, $params->{start});
+        } elsif ($params->{end}) {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM `match` m, game g, teamGame tg WHERE tg.gameId = ? AND tg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` < ?', {}, $params->{gameId}, $params->{end});
+        } else {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM teamGame tg WHERE tg.gameId = ?', {}, $params->{gameId})
+        }
+    }
+
+    if ($params->{teamId}) {
+        if ($params->{start} && $params->{end}) {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM `match` m, game g, teamGame tg WHERE tg.teamId = ? AND tg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ? AND m.`dateTime` < ?', {}, $params->{teamId}, $params->{start}, $params->{end});
+        } elsif ($params->{start}) {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM `match` m, game g, teamGame tg WHERE tg.teamId = ? AND tg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ?', {}, $params->{teamId}, $params->{start});
+        } elsif ($params->{end}) {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM `match` m, game g, teamGame tg WHERE tg.teamId = ? AND tg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` < ?', {}, $params->{teamId}, $params->{end});
+        } else {
+            return $dbh->selectall_arrayref('SELECT tg.* FROM teamGame tg WHERE tg.teamId = ?', {}, $params->{teamId})
+        }
+    }
+}
+
+sub player_game {
+    my ($self, $params) = @_;
+    my $dbh = $self->dbh;
+
+    if ($params->{gameId}) {
+        if ($params->{start} && $params->{end}) {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM `match` m, game g, playerGame pg WHERE pg.gameId = ? AND pg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ? AND m.`dateTime` < ?', {}, $params->{gameId}, $params->{start}, $params->{end});
+        } elsif ($params->{start}) {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM `match` m, game g, playerGame pg WHERE pg.gameId = ? AND pg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ?', {}, $params->{gameId}, $params->{start});
+        } elsif ($params->{end}) {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM `match` m, game g, playerGame pg WHERE pg.gameId = ? AND pg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` < ?', {}, $params->{gameId}, $params->{end});
+        } else {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM playerGame pg WHERE pg.gameId = ?', {}, $params->{gameId})
+        }
+    }
+
+    if ($params->{playerId}) {
+        if ($params->{start} && $params->{end}) {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM `match` m, game g, playerGame pg WHERE pg.playerId = ? AND pg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ? AND m.`dateTime` < ?', {}, $params->{playerId}, $params->{start}, $params->{end});
+        } elsif ($params->{start}) {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM `match` m, game g, playerGame pg WHERE pg.playerId = ? AND pg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` > ?', {}, $params->{playerId}, $params->{start});
+        } elsif ($params->{end}) {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM `match` m, game g, playerGame pg WHERE pg.playerId = ? AND pg.gameId = g.id AND g.matchId = m.id AND m.`dateTime` < ?', {}, $params->{playerId}, $params->{end});
+        } else {
+            return $dbh->selectall_arrayref('SELECT pg.* FROM playerGame pg WHERE pg.playerId = ?', {}, $params->{playerId})
+        }
+    }
+}
+
+sub set_week_params {
+    my $params = shift;
+
+    my $today   = DateTime->today;
+    my $weekday = $today->day_of_week;
+
+    my $subtracted  = ($weekday + 3) % 7;
+    my $start       = $today->subtract(days => $subtracted);
+    $start = $start->ymd;
+
+    $params->{start}    = "$start";
+    $params->{end}      = "";
+}
+
 sub team_score {
     my ($self, $params) = @_;
     my $dbh = $self->dbh;
 
-    return $dbh->selectrow_arrayref('SELECT * FROM teamGame WHERE gameId = ?', {}, $params->{gameId})
-        if ($params->{gameId});
+    # Restrict the games returned to the previous week of games
+    set_week_params $params;
+    # It makes no sense to return the score obtained for both teams
+    $params->{gameId} = "";
+    
+    my $points = 0;
+    my $tgs = $self->team_game($params);
+    foreach my $tg (@$tgs) {
+        my ($tid, $gid, $bk, $dk, $fb, $ft, $fi, $tk) = @$tg;
+        my ($winner) = $dbh->selectrow_array('SELECT winnerId FROM game g WHERE id = ?', {}, $gid);
+        $points += 2 if ($winner == $tid);
+        $points += 2 * $fb + 1 * $tk + 2 * $bk + 1 * $dk;
+    }
 
-    return $dbh->selectall_arrayref('SELECT * FROM teamGame WHERE teamId = ?', {}, $params->{teamId})
-        if ($params->{teamId});
+    return $points;
 }
 
 sub player_score {
     my ($self, $params) = @_;
     my $dbh = $self->dbh;
 
-    return $dbh->selectrow_arrayref('SELECT * FROM playerGame WHERE gameId = ?', {}, $params->{gameId})
-        if ($params->{gameId});
+    set_week_params $params;
+    $params->{gameId} = "";
+    
+    my $points = 0;
+    my $pgs = $self->player_game($params);
+    foreach my $pg (@$pgs) {
+        my ($pid, $gid, $k, $d, $a, $cs, $dk, $tk, $qk, $pk) = @$pg;
+        $points += 2 * $k - 0.5 * $d + 1.5 * $a + 0.01 * $cs + 2 * $tk + 5 * $qk + 10 * $pk;
+        $points += 2 if ($k >= 10 || $a >= 10);
+    }
 
-    return $dbh->selectall_arrayref('SELECT * FROM playerGame WHERE playerId = ?', {}, $params->{playerId})
-        if ($params->{playerId});
+    return $points;
 }
 
 sub match {
--- www/index.pl
+++ www/index.pl
@@ -6,43 +6,56 @@ print header;
 print start_html "League of Legends Public API";
 
 print h1("League of Legends Public API"), "\n";
-print p("This is an experimental API for Fantasy LCS data. Feedback is very welcome, please <a href='http://ryft.co.uk/out.php?target=email'>email</a> me with any suggestions."), "\n";
+print p("This is an experimental API for <a href='http://fantasy.lolesports.com'>Fantasy LCS</a> data. Feedback is very welcome, please <a href='http://ryft.co.uk/out.php?target=email'>email</a> me with any suggestions."), "\n";
 print p("All the source code involved, including the API, database schema and stats processing, is available on <a href='https://github.com/ryft/FantasyLCS'>Github</a>."), "\n";
+print p("Until further notice, all functions are subject to change and/or rate-limiting."), "\n";
 
 print h2("Functions"), "\n";
 print p("All endpoints are URIs which take one or more GET parameters and return a JSON-formatted string."), "\n";
 print p("For example, the request <tt>lol.ryft.co.uk/api/player.json?id=281</tt> returns <tt>[\"281\",\"Tabzz\",\"AD Carry\"]</tt>."), "\n";
+print p("Unless otherwise stated, parameters can't be combined. Lower-bound dates are inclusive, upper bounds exclusive."), "\n";
+print p("Dates must be provided in the format YYYY-MM-DD."), "\n";
 
 my $functions = {
     'team.json' => {
         description => 'Returns the id, name, acronym, and current number of wins and losses of a single team. Useful for finding team IDs and their current success rate.',
-        parameters  => [{ name => 'id', desc => 'The Riot ID of the team.' },
+        parameters  => [{ name => 'id', desc => 'The Riot ID of a team.' },
                         { name => 'acronym', desc => 'The acronym which represents the team name.' }],
     },
     'player.json' => {
         description => 'Returns the id, name, and most recently-played role for a single player. Useful for finding the ID of a player by name.',
-        parameters  => [{ name => 'id', desc => 'The Riot ID of the player.' },
-                        { name => 'name', desc => 'The in-game name of the player.' }],
+        parameters  => [{ name => 'id', desc => 'The Riot ID of a player.' },
+                        { name => 'name', desc => 'The in-game name of a player.' }],
     },
-    'teamScore.json' => {
+    'teamGame.json' => {
         description => 'Returns the following vector for each selected game: <pre>[teamId, gameId, baronsKilled, dragonsKilled, firstBlood, firstTower, firstInhibitor, towersKilled]</pre>
                         Useful for calculating fantasy points for a particular team.',
-        parameters  => [{ name => 'gameId', desc => 'The ID of a single game to be output.' },
-                        { name => 'teamId', desc => 'The ID of a team. All games for this team will be returned.' }],
+        parameters  => [{ name => 'gameId', desc => 'The ID of a game. Both competing teams will be returned.' },
+                        { name => 'teamId', desc => 'The ID of a team. All games for the team will be returned.' },
+                        { name => 'start / end', desc => 'Bounds on match dates. Must also specify gameId or teamId.' }],
     },
-    'playerScore.json' => {
-        description => 'Returns the following vector for each selected game: <pre>[playerId, gameId, kills, deaths, assists, minionKills, tripleKills, quadraKills, pentaKills]</pre>
+    'playerGame.json' => {
+        description => 'Returns the following vector for each selected game: <pre>[playerId, gameId, kills, deaths, assists, minionKills, doubleKills, tripleKills, quadraKills, pentaKills]</pre>
                         Useful for calculating fantasy points for a particular player.',
-        parameters  => [{ name => 'gameId', desc => 'The ID of a single game to be output.' },
-                        { name => 'playerId', desc => 'The ID of a player. All games for this player will be returned.' }],
+        parameters  => [{ name => 'gameId', desc => 'The ID of a game. All players involved in the game be returned.' },
+                        { name => 'playerId', desc => 'The ID of a player. All games for the player will be returned.' },
+                        { name => 'start / end', desc => 'Bounds on match dates. Must also specify gameId or playerId.' }],
     },
     'match.json' => {
         description => 'Returns the following vector for each selected match: <pre>[id, tournamentId, tournamentRound, blueId, redId, winnerId, name, dateTime]</pre>
                         Useful for scoring teams and calculating team fantasy points.',
         parameters  => [{ name => 'id', desc => 'The ID of a single match to be output.' },
-                        { name => 'gameId', desc => 'The ID of a game. The match which included this game ID will be returned.' },
-                        { name => 'start', desc => 'All matches from this date onwards, in YYYY-MM-DD format. Inclusive lower bound, can be combined with \'end\'.' },
-                        { name => 'end', desc => 'All matches up to this date, in YYYY-MM-DD format. Exlusive upper bound, can be combined with \'start\'.' }],
+                        { name => 'gameId', desc => 'The ID of a game. The match which includes this game ID will be returned.' },
+                        { name => 'start', desc => 'Inclusive lower bound on match dates, can be combined with \'end\'.' },
+                        { name => 'end', desc => 'Upper bound on match dates, can be combined with \'start\'.' }],
+    },
+    'playerScore.json' => {
+        description => 'Returns the number of fantasy points accrued by the specified player over the past week of LCS games.',
+        parameters  => [{ name => 'playerId', desc => 'The ID of a player.' }],
+    },
+    'teamScore.json' => {
+        description => 'Returns the number of fantasy points accrued by the specified team over the past week of LCS games.',
+        parameters  => [{ name => 'playerId', desc => 'The ID of a team.' }],
     },
 };
 
